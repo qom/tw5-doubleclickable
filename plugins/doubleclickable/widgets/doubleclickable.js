b2058a7c5bbceeb462b2c787beb72aed420c9352
om <om@_>
2018-02-19 12:17:10 -0500

    Add doubleclickable widget plugin.

---
+++ plugins/doubleclickable/widgets/doubleclickable.js
@@ -0,0 +1,89 @@
+/*\
+title: $:/om/modules/widgets/doubleclickable.js
+type: application/javascript
+module-type: widget
+
+DoubleClickable widget
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+
+var DoubleClickableWidget = function(parseTreeNode,options) {
+	this.initialise(parseTreeNode,options);
+};
+
+/*
+Inherit from the base widget class
+*/
+DoubleClickableWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+DoubleClickableWidget.prototype.render = function(parent,nextSibling) {
+	var self = this;
+	// Remember parent
+	this.parentDomNode = parent;
+	// Compute attributes and execute state
+	this.computeAttributes();
+	this.execute();
+	var tag = this.parseTreeNode.isBlock ? "div" : "span";
+	
+	// Create element and assign classes
+	var domNode = this.document.createElement(tag),
+		classes = (this["class"] || "").split(" ");
+	classes.push("tc-doubleclickable");
+	domNode.className = classes.join(" ");
+	// Add event handlers
+	$tw.utils.addEventListeners(domNode,[
+		{name: "dblclick", handlerObject: this, handlerMethod: "handleDoubleClickEvent"}
+	]);
+	// Insert element
+	parent.insertBefore(domNode,nextSibling);
+	this.renderChildren(domNode,null);
+	this.domNodes.push(domNode);
+	// Stack of outstanding enter/leave events
+	this.currentlyEntered = [];
+};
+
+DoubleClickableWidget.prototype.handleDoubleClickEvent  = function(event) {
+    this.performActions(this.getVariable("currentTiddler"), event);
+	return false;
+};
+
+DoubleClickableWidget.prototype.performActions = function(title,event) {
+	if(this.doubleclickableActions) {
+		this.invokeActionString(this.doubleclickableActions,this,event,{clickedTiddler: title});
+	}
+};
+
+/*
+Compute the internal state of the widget
+*/
+DoubleClickableWidget.prototype.execute = function() {
+	this.doubleclickableActions = this.getAttribute("actions");
+	// Make child widgets
+	this.makeChildWidgets();
+};
+
+/*
+Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
+*/
+DoubleClickableWidget.prototype.refresh = function(changedTiddlers) {
+	var changedAttributes = this.computeAttributes();
+	if(changedAttributes["class"] || changedAttributes.tag) {
+		this.refreshSelf();
+		return true;
+	}
+	return this.refreshChildren(changedTiddlers);
+};
+
+exports.doubleclickable = DoubleClickableWidget;
+
+})();
